dd4e18968138e41178d7de1376e213da3c3ab5d6
Meshcheryakov Andrey <31518980+Merakaa@users.noreply.github.com>
2021-10-27 15:52:52 +0500

    $fix: remove odd

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-const burger = document.querySelector(`.burger`)
-const navMobile = document.querySelector(`.nav__list`)
-burger.addEventListener(`click`, function () {
-    burger.classList.toggle(`burger--active`)
-    navMobile.classList.toggle(`nav__list--opened`)
-})
-document.querySelectorAll(`.phone`).forEach((phone) => {
-    phone.setAttribute(`href`, `tel:+${phone.innerHTML.match(/\d/g).join(``)}`)
-})
